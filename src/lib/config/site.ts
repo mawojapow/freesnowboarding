ced007c6ea6a1f76b1da3712d7ef2f29f9b3fb65
mawojapow <157495156+mawojapow@users.noreply.github.com>
2024-02-08 16:43:03 +0100

    Update site.ts

--- src/lib/config/site.ts
+++ src/lib/config/site.ts
@@ -11,7 +11,7 @@ export const site: SiteConfig = {
     avatar: '/assets/nobodies.png',
     name: 'Freesnowboarding',
     status: '🏴‍☠️',
-    bio: '<br/> just a piece of a noname story <br/> nothin4free $ snow <br/> Humans Music Movies <br/><br/> Snowboarding <br/> <br/>🧩<br/><br/> <a href=https://www.youtube.com/watch?v=j8V-h2VVtvM>Ride</a> </center> free, stick together. <center>'
+    bio: '<br/> just a piece of a noname story <br/> Humans Music Movies <br/><br/> Snowboarding <br/> <br/>🧩<br/><br/> <a href=https://www.youtube.com/watch?v=j8V-h2VVtvM>Ride</a> </center> free, stick together. <center> <br/><br/> nothin4free$snow '
   },
   themeColor: '#3D4451'
 }
