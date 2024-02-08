23005f4cdd6d73a353e3ce61e4348eb7e806bb33
mawojapow <157495156+mawojapow@users.noreply.github.com>
2024-02-08 15:12:34 +0100

    Update site.ts

--- src/lib/config/site.ts
+++ src/lib/config/site.ts
@@ -4,7 +4,7 @@ export const site: SiteConfig = {
   protocol: import.meta.env.URARA_SITE_PROTOCOL ?? import.meta.env.DEV ? 'http://' : 'https://',
   domain: import.meta.env.URARA_SITE_DOMAIN ?? 'localhost:5173',
   title: 'freesnowboarding.xyz',
-  subtitle: 'we RIDE FREE 4 Sisters and Brothers',
+  subtitle: '4 Sisters and Brothers',
   lang: 'en-US',
   description: 'Powered by SvelteKit/Urara',
   author: {
