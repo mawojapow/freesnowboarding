f33be6b0d1c120a0554ee31ba84d79a316aa1286
mawojapow <157495156+mawojapow@users.noreply.github.com>
2024-02-08 15:59:46 +0100

    Update site.ts

--- src/lib/config/site.ts
+++ src/lib/config/site.ts
@@ -2,7 +2,7 @@ import type { SiteConfig } from '$lib/types/site'
 
 export const site: SiteConfig = {
   protocol: import.meta.env.URARA_SITE_PROTOCOL ?? import.meta.env.DEV ? 'http://' : 'https://',
-  domain: import.meta.env.URARA_SITE_DOMAIN ?? 'localhost:5173',
+  domain: import.meta.env.URARA_SITE_DOMAIN ?? 'freesnowboarding.xyz',
   title: 'freesnowboarding.xyz',
   subtitle: '4 Sisters and Brothers',
   lang: 'en-US',
